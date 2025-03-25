5b287d7eeb049946ac34ff92d1dc9511caf045e2
Shubham Naik <shubhamnaik2609>
2025-03-25 14:47:56 -0400

    App setup with Homepage

---
+++ src/pages/Employees.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { GridComponent, ColumnsDirective, ColumnDirective, Page, Selection, Inject, Edit, Toolbar, Sort, Filter } from '@syncfusion/ej2-react-grids';
+import { customersData, customersGrid } from '../data/dummy';
+import { Header } from '../components';
+
+const Employees = () => {
+  return (
+    <div>Employees</div>
+  )
+}
+
+export default Employees
