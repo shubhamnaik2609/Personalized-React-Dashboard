3acdd98dfd850dceaf7c2194fa6d498fd1f18930
Shubham Naik <shubhamnaik2609>
2025-03-25 15:47:01 -0400

    Implemented Customers and Employees Pages

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
-import { GridComponent, ColumnsDirective, ColumnDirective, Page, Selection, Inject, Edit, Toolbar, Sort, Filter } from '@syncfusion/ej2-react-grids';
-import { customersData, customersGrid } from '../data/dummy';
+import { GridComponent, Inject, ColumnsDirective, ColumnDirective, Search, Page, Toolbar } from '@syncfusion/ej2-react-grids';
+import { employeesData, employeesGrid } from '../data/dummy';
 import { Header } from '../components';
 
 const Employees = () => {
   return (
-    <div>Employees</div>
-  )
-}
-
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Page" title="Employees" />
+      <GridComponent
+        dataSource={employeesData}
+        allowPaging
+        allowSorting
+        toolbar={['Search']}
+        width='auto'
+      >
+        <ColumnsDirective>
+          {employeesGrid.map((item, index) => 
+          <ColumnDirective key={index} {...item} />)}
+        </ColumnsDirective>
+        <Inject services={[Page, Search, Toolbar]} />
+      </GridComponent>
+    </div>
+  );
+};
 export default Employees
