e8bfab431e75d50ec27474a9179641d3b104cec6
Shubham Naik <shubhamnaik2609>
2025-03-27 21:06:50 -0400

    Theme Settings bar implementation

--- src/App.js
+++ src/App.js
@@ -43,6 +43,7 @@ const App = () => {
             </div>
           
           <div>
+            <ThemeSettings />
             <Routes>
               {/* Dashboard */}
               <Route path='/' element={(<Ecommerce />)}/>
